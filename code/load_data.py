110842c8ceaa763d4492f90cf0bc3baad82ba443
zydingg <102214148+zydingg@users.noreply.github.com>
2022-10-12 14:23:03 +0800

    Update load_data.py

--- code/load_data.py
+++ code/load_data.py
@@ -4,6 +4,8 @@ import pandas as pd
 
 
 
+
+
 def tokenize(path):
     data_frame = path
     data_columns = data_frame.columns.tolist()
@@ -11,10 +13,9 @@ def tokenize(path):
     data_frame.columns = data_columns
     traindata = data_frame[data_frame.columns[2:]]
     trainlabel = data_frame[data_frame.columns[0]]
-#     proBert_seq = data_frame[data_frame.columns[1]]
-    return np.array(traindata),np.array(trainlabel)
+    proBert_seq = data_frame[data_frame.columns[1]]
+    return np.array(traindata),np.array(trainlabel), np.array(proBert_seq) 
 
 
-
-train_X,train_Y  = tokenize(train_data_path)   
-testX,testY = tokenize(test_data_path)
+train_X,train_Y, proBer_train_seq  = tokenize(train_data_path)   
+testX,testY, proBer_test_seq = tokenize(test_data_path)
