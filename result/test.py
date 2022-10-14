0b0fd69d2881e057ceb95cf31bc8e3ca2aa69ccd
zydingg <102214148+zydingg@users.noreply.github.com>
2022-10-14 13:37:02 +0800

    Update test.py

--- result/test.py
+++ result/test.py
@@ -1,1 +1,32 @@
+import torch
+import torch.nn as nn
+import os
+from torch.utils.data import DataLoader, TensorDataset, Dataset
+import warnings
+import re  
+import numpy as np
+import pandas as pd
+import joblib
+from torch.autograd import Variable
+import torch.optim as optim
+import torch.nn.functional as F
+import random
+import math
 
+
+
+
+
+new_model = BiLSTM_Attention().to(device)      
+new_model.load_state_dict(torch.load("./Deepro_Glu.pkl"))      
+
+test_dataset = AQYDataset(proBer_test_seq, testX, testY, device)
+test_loader = DataLoader(test_dataset,
+              batch_size=12,
+              shuffle=False,
+              drop_last=True,
+              num_workers=4)
+
+
+print("test")
+test_score, _, _  = validate(new_model, test_loader, device)
